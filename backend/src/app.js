7b7833837353c902639b4aea698b40beecbf010e
Adriano Silva Santos (MTZ - RAdriano.santosD - GOVBR) <adriano.santos@govbr.com.br>
2025-01-14 19:51:25 -0300

    Criação de rotas para criar, ler e atualizar pictures

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,62 +2,71 @@ import express from "express";
 
 import cors from 'cors';
 
-const app = express()
+const app = express();
 const PORT = process.env.PORT || 3000;
-const HOSTNAME = '127.0.0.1'
+const HOSTNAME = '127.0.0.1';
 
-const galerydb = [{
-    tbuser:
-        [{ id: "1", name: "Adriano", username: "Tomahok", password: "123456" },
+const users = [
+        { id: "1", name: "Adriano", username: "Tomahok", password: "123456" },
         { id: "2", name: "André", username: "Toliba", password: "125644" },
         { id: "3", name: "Eduardo", username: "Dudu", password: "124777" }]
-},
 
-{
-    tbgalery:
+
+const gallery = 
         [{ id: "1", title: "Space X", image: "https://img.freepik.com/fotos-gratis/abelharucos-coloridos-compartilhando-comida-no-galho-da-arvore_181624-29847.jpg?t=st=1734524933~exp=1734528533~hmac=2d3c72a834e817fba5a2a57c5d29f3869839de5de88c580a4959db9d94749b1a&w=996", description: "The image is the rocket came back to the earth" },
         { id: "2", title: "Boston Dinamic", image: "https://img.freepik.com/fotos-gratis/abelharucos-com-penas-multicoloridas-sentados-no-galho-de-uma-arvore_181624-30537.jpg?t=st=1734525001~exp=1734528601~hmac=7a4877eb853f4c50f895e68c2f316f14a922a154d5ef983d32733c4cc26ba2eb&w=1060", description: "a robot humanoid learning cook with happyBirds" },
         { id: "3", title: "My Family", image: "https://img.freepik.com/fotos-gratis/close-de-um-abelharuco-em-um-galho-de-arvore-sob-a-luz-do-sol_181624-21543.jpg?t=st=1734525027~exp=1734528627~hmac=eec429487ebb98cea46dc0204c487c82b70e415c80026912959e4aca1684a49c&w=996", description: "Dinner with my family, and drink a beer" }]
-}
-];
+
 
 app.use(cors({
     origin: 'http://localhost:5173'
 }))
 
+app.use(express.json());
+
+function buscaImagem(id) {
+    return gallery.findIndex(picture => {
+      return picture.id === id;
+    })
+  }
+
 app.get('/', (req, res) => {
-    res.status(201).send('Hello API!');
+    res.status(200).send('Hello API!');
 });
 
 
 app.get('/api/users', (req, res) => {
-    res.status(201).send(galerydb[0]);
+    res.status(200).json(users);
 });
 
 
 app.get('/api/users/:id/:password/:username', (req, res) => {
     //res.status(201).send()
     console.log(req.params.id);
-})
+});
 
 
 app.get('/api/pictures', (req, res) => {
-    res.status(201).send(galerydb[1])
-})
+    res.status(201).json(gallery);
+});
 
 
 app.get('/api/pictures/:id', (req, res) => {
-    console.log(typeof (req.params.id));
-    const id = parseInt(req.params.id);
-    console.log(typeof (id));
-
-
-
+    const index = buscaImagem(req.params.id);
+    res.status(200).json(gallery[index]);
 });
+
+app.post('/api/pictures', (req, res) => {
+    gallery.push(req.body);
+    res.status(201).send("registro cadastrado com sucesso");
+});
+
+app.put('/api/pictures/:id', (req, res) => {
+    const index = buscaImagem(req.params.id);
+    gallery[index].title = req.body.title;
+    res.status(200).json(gallery);
+})
 
 app.listen(PORT, () => {
     console.log(`app running at http://${HOSTNAME}:${PORT}`)
-})
-
-
-
+});
